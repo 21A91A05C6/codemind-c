c270583187ad041df10c10b19649e345b2b839bf
Manju-Komarapuri <97424965+21A91A05C6@users.noreply.github.com>
2022-06-05 23:35:13 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,t,flag=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d",&t);     for(i=0;i<n;i++)     {          if(arr[i]==t)          {              flag=1;              printf("%d ",i);              break;          }
        }
        for(i=n-1;i>=0;i--)
        {
             if(arr[i]==t)
             {
                 flag=1;
                 printf("%d ",i);
                 break;
             }
             
        }
        if(flag==0)
        {
            printf("-1 -1");
        }
    }

---
+++ _Find_First_and_Last_Position_of_Element_in_Sorted_Array.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,t,flag=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&t);
+    for(i=0;i<n;i++)
+    {
+         if(arr[i]==t)
+         {
+             flag=1;
+             printf("%d ",i);
+             break;
+         }
+         
+    }
+    for(i=n-1;i>=0;i--)
+    {
+         if(arr[i]==t)
+         {
+             flag=1;
+             printf("%d ",i);
+             break;
+         }
+         
+    }
+    if(flag==0)
+    {
+        printf("-1 -1");
+    }
+}
