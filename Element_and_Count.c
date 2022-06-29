01dff69d05c22f8a0614bfb7ab082aa7322f98a9
Manju-Komarapuri <97424965+21A91A05C6@users.noreply.github.com>
2022-06-29 20:25:15 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,j,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=1;         for(j=1;j<n;j++)         {             if(arr[i]==arr[j] && i!=j)             {                 c++;                 arr[j]=-1;             }         }         if(arr[i]!=-1)         {             printf("%d %d ",arr[i],c);         }         //printf("%d %d ",arr[i],c);     } }

---
+++ Element_and_Count.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,j,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=1;
+        for(j=1;j<n;j++)
+        {
+            if(arr[i]==arr[j] && i!=j)
+            {
+                c++;
+                arr[j]=-1;
+            }
+        }
+        if(arr[i]!=-1)
+        {
+            printf("%d %d ",arr[i],c);
+        }
+        //printf("%d %d ",arr[i],c);
+    }
+}
